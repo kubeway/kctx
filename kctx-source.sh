085b1310f6b047193bf10c1571101987739527db
kubeway <kubeway@gmail.com>
2020-04-05 22:14:30 +0800

    bugfix(tmux): fix tmux panes bug

--- kctx-source.sh
+++ kctx-source.sh
@@ -5,12 +5,25 @@
 
 BASEDIR=$(dirname "$0")
 
+##################### set kube context tmp file path #####################
+SAVE_BASE_DIR="/tmp/"
+KUBECONFIG_FILE_PATH="${SAVE_BASE_DIR}/${TERM_SESSION_ID}"
+NAMESPACE_FILE_PATH="${SAVE_BASE_DIR}/${TERM_SESSION_ID}"
+if [ ! -z "${TMUX_PANE}" ]; then
+  KUBECONFIG_FILE_PATH="${KUBECONFIG_FILE_PATH}-${TMUX_PANE}"
+  NAMESPACE_FILE_PATH="${NAMESPACE_FILE_PATH}-${TMUX_PANE}"
+fi
+KUBECONFIG_FILE_PATH="${KUBECONFIG_FILE_PATH}-kubeconfig"
+NAMESPACE_FILE_PATH="${NAMESPACE_FILE_PATH}-namespace"
+
+##################### Functions #####################
+SAVE_BASE_DIR="/tmp/"
 kube_ctx() {
   # symbol=""
   kubeconfig=""
   ns=""
-  if [ -f "/tmp/${TERM_SESSION_ID}-kubeconfig" ]; then
-    f=$(cat "/tmp/${TERM_SESSION_ID}-kubeconfig")
+  if [ -f "${KUBECONFIG_FILE_PATH}" ]; then
+    f=$(cat "${KUBECONFIG_FILE_PATH}")
     if [ ! -z "${f}" ]; then
       if [ -f "${f}" ]; then
         _kubeconfig=$(basename $f)
@@ -21,8 +34,8 @@ kube_ctx() {
     fi
   fi
 
-  if [ -f "/tmp/${TERM_SESSION_ID}-namespace" ]; then
-    _ns=$(cat "/tmp/${TERM_SESSION_ID}-namespace")
+  if [ -f "${NAMESPACE_FILE_PATH}" ]; then
+    _ns=$(cat "${NAMESPACE_FILE_PATH}")
     if [ ! -z "${_ns}" ]; then
       ns="${_ns}"
     fi
@@ -34,8 +47,8 @@ kube_ctx() {
 kubectl_cmd() {
   local args=""
   
-  if [ -f "/tmp/${TERM_SESSION_ID}-kubeconfig" ]; then
-    kubeconfig=$(cat "/tmp/${TERM_SESSION_ID}-kubeconfig")
+  if [ -f "${KUBECONFIG_FILE_PATH}" ]; then
+    kubeconfig=$(cat "${KUBECONFIG_FILE_PATH}")
     if [ ! -z "${kubeconfig}" ]; then
       if [ -f "${kubeconfig}" ]; then
         args="${args} --kubeconfig=${kubeconfig} "
@@ -43,8 +56,8 @@ kubectl_cmd() {
     fi
   fi
 
-  if [ -f "/tmp/${TERM_SESSION_ID}-namespace" ]; then
-    ns=$(cat "/tmp/${TERM_SESSION_ID}-namespace")
+  if [ -f "${NAMESPACE_FILE_PATH}" ]; then
+    ns=$(cat "${NAMESPACE_FILE_PATH}")
     if [ -z "${ns}" ]; then
       ns="default"
     fi
@@ -56,8 +69,8 @@ kubectl_cmd() {
 
 helm_cmd() {
   local args=""
-  if [ -f "/tmp/${TERM_SESSION_ID}-kubeconfig" ]; then
-    kubeconfig=$(cat "/tmp/${TERM_SESSION_ID}-kubeconfig")
+  if [ -f "${KUBECONFIG_FILE_PATH}" ]; then
+    kubeconfig=$(cat "${KUBECONFIG_FILE_PATH}")
     if [ ! -z "${kubeconfig}" ]; then
       if [ -f "${kubeconfig}" ]; then
         args="${args} --kubeconfig=${kubeconfig} "
@@ -65,8 +78,8 @@ helm_cmd() {
     fi
   fi
 
-  if [ -f "/tmp/${TERM_SESSION_ID}-namespace" ]; then
-    ns=$(cat "/tmp/${TERM_SESSION_ID}-namespace")
+  if [ -f "${NAMESPACE_FILE_PATH}" ]; then
+    ns=$(cat "${NAMESPACE_FILE_PATH}")
     if [ -z "${ns}" ]; then
       ns="default"
     fi
